0d10d76f17b772c4fba9812a15f82a0c096563a6
Gautam Dhodi <gautamdhodi02@gmail.com>
2025-02-25 21:53:04 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -10,6 +10,10 @@ app.use("/hi",(req,res)=>{
     res.send("hello")
 })
 
+app.use("/try",(req,res)=>{
+    res.send("try and testes api")
+})
+
 app.listen(1111,()=>{
     console.log("Server has started on port 1111 .....")
 })

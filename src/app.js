e5b996a50a8fc279452b3273b11f674b7646262f
Gautam Dhodi <gautamdhodi02@gmail.com>
2025-02-26 22:55:28 +0530

    Push some data to database

--- src/app.js
+++ src/app.js
@@ -2,7 +2,22 @@ const express = require('express');
 
 const app = express();
 
-const connectDB = require("./config/database")
+const connectDB = require("./config/database");
+const user = require('./models/user');
+
+app.post("/signup", async (req,res)=>{
+    const user = new user( {
+        firstName : "Gautam",
+        lastName : "Dhodi",
+        emailId : "gautamdhodi02@gmail.com",
+        password : "gautam123",
+        age : 20,
+        gender : "male",
+    })
+    await user.save();
+    res.send("User added successfully ....")
+})
+
 
 connectDB()
     .then( ()=>{
@@ -12,7 +27,8 @@ connectDB()
         console.error("Database cannot be connected");
     });
       
-app.listen(1111,()=>{
-    console.log("Server has started on port 1111 .....")
-})
+    app.listen(1111,()=>{
+        console.log("Server has started on port 1111 .....")
+    })
+
  

9b418e2bd9165fe3b040a9bd85cf17404033f2e8
Gautam Dhodi <gautamdhodi02@gmail.com>
2025-03-03 22:56:39 +0530

    Created some schema methods for user

--- src/app.js
+++ src/app.js
@@ -55,14 +55,12 @@ app.post("/login", async (req,res) => {
         if(!user){
             throw new Error("Invalid Credentials ")
         }
-        const isPasswordValid =  await bcrypt.compare(password, user.password);
+        const isPasswordValid =  await user.validatePassword(password);
 
         if(isPasswordValid){
 
             // Create a JWT Token
-            const token = await jwt.sign({_id: user._id}, "DEV@Tinder$790",{
-                expiresIn: '1d',
-            });
+            const token = await user.getJWT();
 
             // Add the token to the cookie and sendthe response back to the server
             res.cookie("token",token,{

95941c44afd7edaae18e4ad45fd3234a369e4509
Gautam Dhodi <gautamdhodi02@gmail.com>
2025-03-06 11:05:56 +0530

    prepare interested POST api

--- src/routes/request.js
+++ src/routes/request.js
@@ -48,6 +48,7 @@ requestsRouter.post(
             status,
         });
 
+
         const data = await connectionRequest.save();
 
         res.json({
@@ -62,4 +63,50 @@ requestsRouter.post(
    // res.send(user.firstname + "Sent the connection request")
 })
 
+
+requestsRouter.post(
+    "/request/review/:status/:requestId", 
+    userAuth, 
+    async (req,res)=>{
+        try {
+            // user info
+            const loggedInUser = req.user;
+            const {status, requestId} = req.params;
+            const allowedStatus = ["accepted", "rejected"];
+            if(!allowedStatus.includes(status)) {
+                return res
+                    .status(400)
+                    .json({
+                        message: "Status not allowed"
+                    })
+            }
+
+            // Checking If request id is present
+            const connectionRequest = await ConnectionRequest.findOne({
+                _id: requestId,
+                toUserId: loggedInUser._id,
+                status: "interested",
+            })
+
+            if(!connectionRequest) {
+                return res
+                    .status(404)
+                    .json({
+                        message:"Connection request not found"
+                    })
+            }
+
+            connectionRequest.status = status;
+            
+            const data = await connectionRequest.save();
+            
+            res.json({
+                message:"Connection Request " + status,
+                data
+            });
+        } catch (err) {
+            res.status(400).send("ERROR : " + err.message);
+        }
+    }
+)
 module.exports = requestsRouter;

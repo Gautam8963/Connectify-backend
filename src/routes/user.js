579d3879a3c2b0dc46e3d166974d130458ea6b2c
Gautam Dhodi <gautamdhodi02@gmail.com>
2025-03-06 13:12:46 +0530

    Build a GET connection api to get the details of all the connections.

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,6 +4,8 @@ const useRouter = express.Router();
 const { userAuth } = require('../middlewares/auth');
 const ConnectionRequest = require('../models/connectionRequest')
 
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
+
 useRouter.get(
     "/user/requests/recieved",
     userAuth,
@@ -15,7 +17,7 @@ useRouter.get(
                 toUserId: loggedInUser._id,
                 status: "interested",
             })
-            .populate("fromUserId", ["firstName","lastName"]);
+            .populate("fromUserId", USER_SAFE_DATA);
 
             res.json({
                 message: "Data fetched successfully",
@@ -26,5 +28,37 @@ useRouter.get(
         }
     }
 )
+
+useRouter.get(
+    "/user/connections",
+    userAuth,
+    async (req,res) => {
+        try {
+            const loggedInUser = req.user;
+
+            const connectionRequests = await ConnectionRequest.find({
+                $or: [
+                    { toUserId: loggedInUser._id, status: "accepted"},
+                    { fromUserId: loggedInUser._id, status: "accepted",}
+                ],
+            })
+            .populate("fromUserId", USER_SAFE_DATA)
+            .populate("toUserId", USER_SAFE_DATA);
+
+            console.log(connectionRequests)
+            
+            const data = connectionRequests.map((row) => {
+                if (row.fromUserId.toString() === loggedInUser.toString()) {
+                    return row.toUserId;
+                }
+                return row.fromUserId;
+            });
+
+            res.json({data});
+        } catch (err) {
+            res.status(400).send({message: err.message});
+        }
+    }
+);
 
 module.exports = useRouter;

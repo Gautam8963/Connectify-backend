c32d0654f6dd1c000d3827238b9161929a4f7ae6
Gautam Dhodi <gautamdhodi02@gmail.com>
2025-03-08 20:01:31 +0530

    some bug fixing

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,112 +1,103 @@
-const express = require('express');
-const { userAuth }= require("../middlewares/auth")
+const express = require("express");
+const requestRouter = express.Router();
 
-const requestsRouter = express.Router();
-const ConnectionRequest = require('../models/connectionRequest');
-const user = require('../models/user');
+const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
-requestsRouter.post(
-    "/request/send/:status/:toUserId",
-    userAuth, 
-    async (req,res) => {
-    
+// const sendEmail = require("../utils/sendEmail");
+
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
     try {
-        const fromUserId = req.user._id;
-        const toUserId = req.params.toUserId;
-        const status = req.params.status;
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
 
-        const allowedStatus = ["ignored","interested"];
-        if (!allowedStatus.includes(status)) {
-            return res
-                .status(400)
-                .json({message: "Invalid Status Type: " + status})
-        }
+      const allowedStatus = ["ignored", "interested"];
+      if (!allowedStatus.includes(status)) {
+        return res
+          .status(400)
+          .json({ message: "Invalid status type: " + status });
+      }
 
-        //If user is not present in DB
-        const toUser = await user.findById(toUserId);
-        if(!toUser) {
-            return res.status(404).json({message: "User not found !" });
-        }
+      const toUser = await User.findById(toUserId);
+      if (!toUser) {
+        return res.status(404).json({ message: "User not found!" });
+      }
 
+      const existingConnectionRequest = await ConnectionRequest.findOne({
+        $or: [
+          { fromUserId, toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+      if (existingConnectionRequest) {
+        return res
+          .status(400)
+          .send({ message: "Connection Request Already Exists!!" });
+      }
 
-        // If Request Already Exists
-        const existingConnectionRequest = await ConnectionRequest.findOne({
-            $or: [
-                { fromUserId, toUserId},
-                {fromUserId : toUserId, toUserId : fromUserId}
-            ],
-        });
-        if(existingConnectionRequest) {
-            return res
-                .status(400)
-                .send({ message: "Connetion Request already exists !!!"})
-        }
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
 
-        const connectionRequest = new ConnectionRequest({
-            fromUserId,
-            toUserId,
-            status,
-        });
+      const data = await connectionRequest.save();
 
+      const emailRes = await sendEmail.run(
+        "A new friend request from " + req.user.firstName,
+        req.user.firstName + " is " + status + " in " + toUser.firstName
+      );
+      console.log(emailRes);
 
-        const data = await connectionRequest.save();
-
-        res.json({
-            message: req.user.firstName+"is "+status+" by/in "+toUser.firstName,
-            data,
-        })
-
+      res.json({
+        message:
+          req.user.firstName + " is " + status + " in " + toUser.firstName,
+        data,
+      });
     } catch (err) {
-        res.status(400).send("ERROR : " + err.message);
+      res.status(400).send("ERROR: " + err.message);
     }
+  }
+);
 
-   // res.send(user.firstname + "Sent the connection request")
-})
+requestRouter.post(
+  "/request/review/:status/:requestId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const loggedInUser = req.user;
+      const { status, requestId } = req.params;
 
+      const allowedStatus = ["accepted", "rejected"];
+      if (!allowedStatus.includes(status)) {
+        return res.status(400).json({ messaage: "Status not allowed!" });
+      }
 
-requestsRouter.post(
-    "/request/review/:status/:requestId", 
-    userAuth, 
-    async (req,res)=>{
-        try {
-            // user info
-            const loggedInUser = req.user;
-            const {status, requestId} = req.params;
-            const allowedStatus = ["accepted", "rejected"];
-            if(!allowedStatus.includes(status)) {
-                return res
-                    .status(400)
-                    .json({
-                        message: "Status not allowed"
-                    })
-            }
+      const connectionRequest = await ConnectionRequest.findOne({
+        _id: requestId,
+        toUserId: loggedInUser._id,
+        status: "interested",
+      });
+      if (!connectionRequest) {
+        return res
+          .status(404)
+          .json({ message: "Connection request not found" });
+      }
 
-            // Checking If request id is present
-            const connectionRequest = await ConnectionRequest.findOne({
-                _id: requestId,
-                toUserId: loggedInUser._id,
-                status: "interested",
-            })
+      connectionRequest.status = status;
 
-            if(!connectionRequest) {
-                return res
-                    .status(404)
-                    .json({
-                        message:"Connection request not found"
-                    })
-            }
+      const data = await connectionRequest.save();
 
-            connectionRequest.status = status;
-            
-            const data = await connectionRequest.save();
-            
-            res.json({
-                message:"Connection Request " + status,
-                data
-            });
-        } catch (err) {
-            res.status(400).send("ERROR : " + err.message);
-        }
+      res.json({ message: "Connection request " + status, data });
+    } catch (err) {
+      res.status(400).send("ERROR: " + err.message);
     }
-)
-module.exports = requestsRouter;
+  }
+);
+
+module.exports = requestRouter;
--- src/routes/user.js
+++ src/routes/user.js
@@ -8,7 +8,7 @@ const ConnectionRequest = require('../models/connectionRequest')
 const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
 
 useRouter.get(
-    "/user/requests/recieved",
+    "/user/requests/received",
     userAuth,
     async (req, res) => {
         try {
@@ -18,14 +18,14 @@ useRouter.get(
                 toUserId: loggedInUser._id,
                 status: "interested",
             })
-            .populate("fromUserId", USER_SAFE_DATA);
+                .populate("fromUserId", USER_SAFE_DATA);
 
             res.json({
                 message: "Data fetched successfully",
                 data: connectionRequests,
             })
         } catch (err) {
-            res.statusCode(400).send("ERROR : " + err.message )
+            res.statusCode(400).send("ERROR : " + err.message)
         }
     }
 )
@@ -33,21 +33,21 @@ useRouter.get(
 useRouter.get(
     "/user/connections",
     userAuth,
-    async (req,res) => {
+    async (req, res) => {
         try {
             const loggedInUser = req.user;
 
             const connectionRequests = await ConnectionRequest.find({
                 $or: [
-                    { toUserId: loggedInUser._id, status: "accepted"},
-                    { fromUserId: loggedInUser._id, status: "accepted",}
+                    { toUserId: loggedInUser._id, status: "accepted" },
+                    { fromUserId: loggedInUser._id, status: "accepted", }
                 ],
             })
-            .populate("fromUserId", USER_SAFE_DATA)
-            .populate("toUserId", USER_SAFE_DATA);
+                .populate("fromUserId", USER_SAFE_DATA)
+                .populate("toUserId", USER_SAFE_DATA);
 
             console.log(connectionRequests)
-            
+
             const data = connectionRequests.map((row) => {
                 if (row.fromUserId.toString() === loggedInUser.toString()) {
                     return row.toUserId;
@@ -55,55 +55,56 @@ useRouter.get(
                 return row.fromUserId;
             });
 
-            res.json({data});
+            res.json({ data });
         } catch (err) {
-            res.status(400).send({message: err.message});
+            res.status(400).send({ message: err.message });
         }
     }
 );
 
-useRouter.get(
-    "/feed",
-    userAuth,
-    async (req,res) => {
-        try {
-            const loggedInUser = req.user;
+useRouter.get("/feed", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
 
-            // Logic for 10 users at a time 
-            const page = parseInt(req.query.page) || 1;
-            const limit = parseInt(req.query.limit) || 10;
-            limit = limit > 50 ? 50 : limit;
+        // Logic for 10 users at a time 
+        let page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
 
-            const skip = (page - 1) * limit;
+        const skip = (page - 1) * limit;
 
-            const connectionRequests = await ConnectionRequest.find({
-                $or :[
-                    {fromUserId: loggedInUser._id},
-                    {toUserId: loggedInUser._id}
-                ],
-            }).select("fromUserId toUserId");
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                { fromUserId: loggedInUser._id },
+                { toUserId: loggedInUser._id }
+            ],
+        }).select("fromUserId toUserId");
 
-            const hideUsersFromFeed = new Set();
-            connectionRequests.forEach((req)=> {
-                hideUsersFromFeed.add(req.fromUserId.toString());
-                hideUsersFromFeed.add(req.toUserId.toString());
-            });
+        const hideUsersFromFeed = new Set();
+        connectionRequests.forEach((req) => {
+            hideUsersFromFeed.add(req.fromUserId.toString());
+            hideUsersFromFeed.add(req.toUserId.toString());
+        });
 
-            // finding all users
-            const users = await User.find({
-                $and: [
-                    {_id: { $nin: Array.from(hideUsersFromFeed) } },
-                    { _id: { $ne: loggedInUser._id }}
-                ]
-            })
+        // finding all users
+        const users = await User.find({
+            $and: [
+                { _id: { $nin: Array.from(hideUsersFromFeed) } },
+                { _id: { $ne: loggedInUser._id } }
+            ]
+        })
             .select(USER_SAFE_DATA)
             .skip(skip)
             .limit(limit);
 
-            res.json({data : users});
-        } catch (err) {
-            res.status(400).json({message: err.message});
+        res.json({
+            message: "Data fethed successfully ",
+            data: users
         }
+        );
+    } catch (err) {
+        res.status(400).json({ message: err.message + "not fetch" });
     }
+}
 );
 module.exports = useRouter;

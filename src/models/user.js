9c4698d1619a1f2ff6dd0db9db5e6e839d10a1ed
Gautam Dhodi <gautamdhodi02@gmail.com>
2025-03-09 11:10:43 +0530

    changes in the userSchema

--- src/models/user.js
+++ src/models/user.js
@@ -1,113 +1,107 @@
 const mongoose = require("mongoose");
-const validator = require('validator');
+const validator = require("validator");
 const jwt = require("jsonwebtoken");
-
 const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     firstName: {
-        type: String,
-        required: true,
-        minLength: 3,
-        maxLength: 15
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50,
     },
     lastName: {
-        type: String,
-        required: true,
-        minLength: 3,
-        maxLength: 15
+      type: String,
+            required: true,
     },
     emailId: {
-        type: String,
-        required: true,
-        unique: true, // This adds the uniqueness constraint at the database level
-        lowercase: true,
-        trim: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error("Email id is not valid"+value)
-            }
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address: " + value);
         }
-        // ,
-        // validate: {
-        //     validator: async function (value) {
-        //         const user = await this.constructor.find({ emailId: value });
-        //         if (user) {
-        //             throw new Error("Email already existszzz.");
-        //         }
-        //     },
-        //     message: "Email already exists."
-        // }
+      },
     },
     password: {
-        type: String,
-        validate(value) {
-            if (!validator.isStrongPassword(value)) {
-                throw new Error("Password is not strong"+value)
-            }
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a Strong Password: " + value);
         }
+      },
     },
     age: {
-        type: Number,
-        validate(value) {
-            if (value < 18 || value > 50) {
-                throw new Error("You are not eligible for this platform");
-            }
-        }
-    }
-    ,
+      type: Number,
+      min: 18,
+    },
     gender: {
+      type: String,
+      enum: {
+        values: ["male", "female", "other"],
+        message: `{VALUE} is not a valid gender type`,
+      },
+      // validate(value) {
+      //   if (!["male", "female", "others"].includes(value)) {
+      //     throw new Error("Gender data is not valid");
+      //   }
+      // },
+    },
+    isPremium: {
+      type: Boolean,
+      default: false,
+    },
+    membershipType: {
+      type: String,
+    },photoUrl: {
         type: String,
-        lowercase: true,
-        validate(value){
-            if (!["male", "female", "others"].includes(value)){
-                throw new Error("Gender data is not valid")
-            }
+        trim: true, // Removes extra spaces at the start or end
+        default: "",
+        validate: {
+          validator: (value) => {
+            return value === "" || validator.isURL(value);
+          },
+          message: (props) => `Invalid Photo URL: ${props.value}`,
         },
-    },
-    createdAt: {
-        type: Date,default: Date.now,
-    },
-    photoUrl: {
-        type: String,
-        default: "https://pinnacle.works/dummy-image/",
-        validate(value) {
-            if (!validator.isURL(value)) {
-                throw new Error("URL is not valid"+value)
-            }
-        }
-    },
+      },
     about: {
-        type: String,
-        default: "This is the about section"
+      type: String,
+      default: "This is a default about of the user!",
     },
     skills: {
-        type: [String],
+      type: [String],
     },
-},
-{
-    timestamps: true
-});
+  },
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.methods.getJWT = async function () {
-    const user = this;
+  const user = this;
 
-    const token = await jwt.sign({ _id: user._id}, "DEV@Tinder$790", {
-        expiresIn: "7d",
-    });
+  const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$790", {
+    expiresIn: "7d",
+  });
 
-    return token;
-}
+  return token;
+};
 
 userSchema.methods.validatePassword = async function (passwordInputByUser) {
-    const user = this;
-    const passwordHash = user.password;
+  const user = this;
+  const passwordHash = user.password;
 
-    const isPasswordValid = await bcrypt.compare(
-        passwordInputByUser,
-        passwordHash
-    )
-    return isPasswordValid
-}
+  const isPasswordValid = await bcrypt.compare(
+    passwordInputByUser,
+    passwordHash
+  );
+
+  return isPasswordValid;
+};
 
 module.exports = mongoose.model("User", userSchema);
